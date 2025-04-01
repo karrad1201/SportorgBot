9313137af65926d8b338a95acf0f217ff3d59a55
Karrad <karad120109@gmail.com>
2025-04-01 16:50:06 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = ''  # Замените на свой токен
+ADMIN_USER_ID = []  # Замените на ID администраторов
+DATABASE_NAME = 'itptoger.sql'
+SECRET_COMMAND = 'users'  # Или другая секретная команда
+
+BAD_WORDS = ["Пенис", "хуйня", "Пидоры"]
